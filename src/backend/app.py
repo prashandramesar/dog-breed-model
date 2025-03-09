9aebea8d78b6f80eeb0e7fa08e71dbbb541bb766
prashandramesar <prashand_87@hotmail.com>
2025-03-09 17:10:10 +0100

    remove debug=True for deployment

--- src/backend/app.py
+++ src/backend/app.py
@@ -111,4 +111,4 @@ def read_root() -> dict[str, str]:
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
